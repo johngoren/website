a06df0d65efae32dedbd51c53e415a9a3c56371f
John Gorenfeld <john@Johns-MacBook-Air-2.local>
2025-09-29 16:43:27 +0100

    website

--- eleventy.config.js
+++ eleventy.config.js
@@ -76,11 +76,11 @@ export default async function(eleventyConfig) {
 		},
 		metadata: {
 			language: "en",
-			title: "John's blog",
-			subtitle: "Bringing back the idea of blogs",
+			title: "Blog Title",
+			subtitle: "This is a longer description about your blog.",
 			base: "https://example.com/",
 			author: {
-				name: "John Gorenfeld"
+				name: "Your Name"
 			}
 		}
 	});
